89e92c3ac0c05fcaf860896f149a08402db6121c
zrh <yohoo324@163.com>
2017-09-01 15:03:56 +0800

    09-01
    OwnWeb

---
+++ ownWeb/js/main.js
@@ -0,0 +1,28 @@
+var backTime=null;
+$(window).scroll(function(){
+	
+	var sTop=document.documentElement.scrollTop||document.body.scrollTop;
+  	if(sTop>=350){
+		$("#backTop").css({"display":"block"});
+		$("#backTopi").css({"display":"block"});
+		
+	}else{
+		$("#backTop").css({"display":"none"});
+		$("#backTopi").css({"display":"none"});
+	}
+});
+$("#backTop").click(function(){
+	var sTop=document.documentElement.scrollTop||document.body.scrollTop;
+	function backT(){
+		backTime=setInterval(function(){
+			sTop-=50;
+			document.documentElement.scrollTop=sTop;
+			document.body.scrollTop=sTop;
+			if(sTop<=0){
+				clearInterval(backTime);
+			}
+		},1)
+	}
+	backT();
+})
+
